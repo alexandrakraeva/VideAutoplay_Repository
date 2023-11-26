f6d2657f41db5f5534fdf3f8a9259aa80a9c942d
alexandrakraeva <66311877+alexandrakraeva@users.noreply.github.com>
2023-11-26 15:10:14 +0100

    added

---
+++ EmptyJSFile.js
@@ -0,0 +1,10 @@
+﻿document.addEventListener('DOMContentLoaded', (event) => {
+    const video = document.getElementById('Cartoon');
+    video.addEventListener('loadedmetadata', () => {
+        video.play();
+    });
+
+    video.onended = function () {
+        // Optional: Redirect or perform an action after the video ends
+    };
+});

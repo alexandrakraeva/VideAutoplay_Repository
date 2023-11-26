bc1181fadac386b8a547abd711b4c95501925a90
alexandrakraeva <66311877+alexandrakraeva@users.noreply.github.com>
2023-11-26 15:22:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
 ﻿document.addEventListener('DOMContentLoaded', (event) => {
     const video = document.getElementById('Cartoon');
-
-    // Play video on user interaction
-    document.body.addEventListener('click', function () {
-        video.play();
-    });
-
     video.addEventListener('loadedmetadata', () => {
-        // Attempt to play video
-        video.play().catch(e => {
-            // Autoplay was prevented
-            // Show a play button to the user, or handle the failure gracefully
-        });
+        video.play();
     });
 
     video.onended = function () {
